b90d770d256460c93249cf3ce77b1e6cb37784f3
huaixiaoz <hello@itmp.top>
2016-09-11 21:24:55 +0800

    add choices.cpp

--- myfirst.cpp
+++ myfirst.cpp
@@ -3,8 +3,22 @@
 //
 
 #include <iostream>
+#include <cmath>
+
 int main(){
     using namespace std;
     cout << "Hello world!!" <<endl;
+//    string str;
+//    cin >> str;
+//
+//    cout << str << endl;
+
+    double area;
+    cout << "Enter the floor area: " << endl;
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "Output: " << side << endl;
+
     return 0;
 }
