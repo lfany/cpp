9390c8b4b58b6b273338134c421bfe3156769654
hello <hello@itmp.top>
2016-09-13 14:33:54 +0800

    add mytime

---
+++ mytime/Time.h
@@ -0,0 +1,26 @@
+//
+// Created by dp on 2016/9/13.
+//
+
+#ifndef HELLO_TIME_H
+#define HELLO_TIME_H
+
+
+class Time {
+
+private:
+    int hours;
+    int minutes;
+
+public:
+    Time();
+    Time(int h, int m = 0);
+    void AddMin(int m);
+    void AddHr(int h);
+    void Reset(int h = 0, int m = 0);
+    Time Sum(const Time &t) const;
+    void Show() const;
+};
+
+
+#endif //HELLO_TIME_H
---
+++ mytime/mytime.cpp
@@ -0,0 +1,36 @@
+//
+// Created by dp on 2016/9/13.
+//
+
+#include <iostream>
+#include "Time.h"
+
+int main(){
+    using std::cout;
+    using std::endl;
+
+    Time planning;
+    Time coding(2, 40);
+    Time fixing(5, 55);
+    Time total;
+
+    cout << "planning time = ";
+    planning.Show();
+    cout << endl;
+
+    cout << "codding time = ";
+    coding.Show();
+    cout << endl;
+
+    cout << "fixing time = ";
+    fixing.Show();
+    cout << endl;
+
+    total = coding.Sum(fixing);
+
+    cout << "coding.Sum(fixing) time = ";
+    total.Show();
+    cout << endl;
+
+    return 0;
+}
